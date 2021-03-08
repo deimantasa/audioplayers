5c6c70e210f53bb9c7ff907512fa948eef840439
Aurimas Deimantas(mac) <aurimas.deimantas@gmail.com>
2021-03-08 14:11:37 +0700

    refactor: update driver

--- example/test_driver/app_test.dart
+++ example/test_driver/app_test.dart
@@ -5,7 +5,7 @@ void main() {
   group('simple functionality', () {
     final url1Finder = find.byValueKey('url1');
 
-    FlutterDriver driver;
+    late FlutterDriver driver;
 
     // Connect to the Flutter driver before running any tests.
     setUpAll(() async {
@@ -14,14 +14,11 @@ void main() {
 
     // Close the connection to the driver after the tests have completed.
     tearDownAll(() async {
-      if (driver != null) {
-        driver.close();
-      }
+      driver.close();
     });
 
     test('simplest test', () async {
-      expect(await driver.getText(url1Finder),
-          'Sample 1 (https://luan.xyz/files/audio/ambient_c_motion.mp3)');
+      expect(await driver.getText(url1Finder), 'Sample 1 (https://luan.xyz/files/audio/ambient_c_motion.mp3)');
     });
   });
 }
